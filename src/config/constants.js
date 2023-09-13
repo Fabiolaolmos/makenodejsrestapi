c5aeb91b81e75b7a33f9b4d564115dda8c9b94c1
Fabiolaolmos <138613359+Fabiolaolmos@users.noreply.github.com>
2023-09-13 22:36:46 +0000

    Configuración de registros

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,6 +1,8 @@
-const devConfig = { MONGO_URL: 'mongodb://localhost/makeanodejsapi-dev', }; 
- const testConfig = { MONGO_URL: 'mongodb://localhost/makeanodejsapi-test', }; 
- const prodConfig = { MONGO_URL: 'mongodb://localhost/makeanodejsapi-prod', };
+const devConfig = { MONGO_URL: 'mongodb://localhost/makenodejsrestapi-dev', }; 
+ const testConfig = { MONGO_URL: 'mongodb://localhost/makenodejsrestapi-test', }; 
+ const prodConfig = { MONGO_URL: 'mongodb://localhost/makenodejsrestapi-prod', };
+
+ 
 const defaultConfig = {
 PORT: process.env.PORT || 3000,
 };
--- src/modules/users/user.routes.js
+++ src/modules/users/user.routes.js
@@ -1,7 +1,7 @@
 import {
     Router
 } from 'express';
-import * as userController from '../user.controllers';
+import * as userController from './user.controllers';
 const routes = new Router();
 routes.post('/signup', userController.signUp);
 export default routes;
