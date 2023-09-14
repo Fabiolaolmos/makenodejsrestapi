767b3a02dbd9b96e4f7aec5b15a991020a81e9db
Fabiolaolmos <138613359+Fabiolaolmos@users.noreply.github.com>
2023-09-14 21:11:18 +0000

    Corrección de erres en el archivo constants.js

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,26 +1,18 @@
-const devConfig = { MONGO_URL: 'mongodb://localhost:27017/makenodejsrestapi-dev', }; 
- const testConfig = { MONGO_URL: 'mongodb://localhost/makenodejsrestapi-test', }; 
- const prodConfig = { MONGO_URL: 'mongodb://localhost/makenodejsrestapi-prod', };
-
- 
-const defaultConfig = {
-PORT: process.env.PORT || 3000,
-
-};
+const devConfig = { MONGO_URL: 'mongodb://localhost:27017/makenodejsrestapi-dev' };
+const testConfig = { MONGO_URL: 'mongodb://localhost/makenodejsrestapi-test' };
+const prodConfig = { MONGO_URL: 'mongodb://localhost/makenodejsrestapi-prod' };
+const defaultConfig = { PORT: process.env.PORT || 3000 };
 function envConfig(env) {
-     switch (env) {
-         case 'development':
-             return devConfig;
-         case 'test':
-             return testConfig;
-         default:
-             return prodConfig;
-     }
- }
- 
- //Take defaultConfig and make it a single object 
- //So, we have concatenated two objects into one 
- export default { ...defaultConfig,
-     ...envConfig(process.env.NODE_ENV),
- };
-  
+  switch (env) {
+    case 'development':
+      return devConfig;
+    case 'test':
+      return testConfig;
+    default:
+      return prodConfig;
+  }
+}
+export default {
+  ...defaultConfig,
+  ...envConfig(process.env.NODE_ENV),
+};
